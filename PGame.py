bad58de5c8eb3c63f04e11a9b98e0ff874c57cb1
kbell <k.bell@cmcmarkets.com>
2018-12-06 15:02:42 +0000

    progress
    Signed-off-by: kbell <k.bell@cmcmarkets.com>

--- PGame.py
+++ PGame.py
@@ -3,32 +3,75 @@ cardPlacement = 0
 placementPoint = 0
 transitCount = 0
 deckCount = 0
+outDeckCount = 0
 
+class CircularQueue():
+    def __init__(self):
+        self.queue = list()
+        self.head = 0
+        self.tail = 0
+        self.maxSize = 52
+    
+    def enqueue(self, data):
+        if (self.size() == self.maxSize-1):
+            return ("queue full")
+        self.queue.append(data)
+        self.tail = (self.tail +1) % self.maxSize
+        return True
+    
+    def dequeue(self):
+       if self.size()==0:
+            return ("Queue Empty!") 
+        data = self.queue[self.head]
+        self.head = (self.head + 1) % self.maxSize
+        return data 
+
+    def seeHead():
+        return self.queue[self.head]
+
+    def size(self):
+        if self.tail>=self.head:
+            return (self.tail-self.head)
+        return (self.maxSize - (self.head-self.tail))
+
+    
 def play(numPlayers):
     #shuffle
     #deal 2 each
     #shuffle player order
+    #you will always be player 0    
 
     for x in range(numPlayers):
         playerStack[x] = x
 
     random.shuffle(playerStack)
 
-    #you will always be player 0
+    deckQ = shuffle() # cards
+    dealOut(playerStack)
+    #intial bet
+    #
 
-    deck = shuffle() # cards
-    transitDeck = []
-  
+
+def dealOut(playerStack):
+    outDeck = []
+
     for y in range(0,2,1):
         for x in playerStack:
-            playerStack[y,x] = deck[cardPlacement]
-            transitDeck[transitCount] = deck[cardPlacement]
-            cardPlacement+=1
+            playerStack[y,x] = deckQ.seeHead()
+            outDeck[outDeckCount] = deckQ.dequeue()
+            outDeckCount+=1
+ 
     
     tableCards = []
-    tableCards[0] = deck
+    for i in range(0,3,1):
+        tableCards[i] = deckQ.seeHead()
+        outDeck[outDeckCount] = deckQ.dequeue()
+        outDeckCount+=1
+
+
 
 def shuffle():
+    deckQ = CircularQueue()
     #0-12 hearts
     #13-25 spades
     #26-38 diamonds
@@ -36,6 +79,8 @@ def shuffle():
     for i in range(0,52,1):
         deckArr[i] = i
     random.shuffle(deckArr)
-    return deckArr
+    for i in deckArr:
+        deckQ.enqueue(deckArr[i])
+    return deckQ
 
  
